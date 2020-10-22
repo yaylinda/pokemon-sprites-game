7ebb0ab694ca34aea5618ff813462d6eb98d4339
Linda Zheng <lindazheng@Lindas-Macbook-Pro.local>
2020-10-21 23:18:38 -0500

    some refactoring/cleanup

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,19 +1,17 @@
-import { Picker } from '@react-native-community/picker';
 import { produce } from 'immer';
 import React, { useCallback, useEffect, useState } from 'react';
 import { Dimensions, Image, Modal, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View } from 'react-native';
 import { Button, Icon, Slider } from 'react-native-elements';
 import { getRandomStarterForType, PokemonData, PokemonType } from './Pokemon';
+import styles from './styles';
 
 const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
 console.log(`[Game] - width: ${WIDTH}, height: ${HEIGHT}`);
 
-const CELL_MARGIN_WIDTH = 4;
 const CELL_BORDER_WIDTH = 1;
 const GAMEBOARD_NUM_ROWS = 10;
 const GAMEBOARD_NUM_COLS = 10;
 const CELL_SIZE: number = (WIDTH - (CELL_BORDER_WIDTH * GAMEBOARD_NUM_COLS * 2)) / GAMEBOARD_NUM_COLS;
-// const STARTING_ENERGY = 5;
 const STARTING_ENERGY = 10;
 const MAX_ENERGY = 10;
 
@@ -37,18 +35,16 @@ class GameboardCellData {
         this.isMoveTarget = false;
         this.isPotentialMoveDestination = false;
         this.pokemonData = undefined;
-        this.ownedByType = undefined; 
+        this.ownedByType = undefined;
     }
 }
 
 type GameState =
     'PENDING_START_GAME' |
     'SELECT_ACTION' |
-    // 'MOVE' | 'ATTACK' |
     'SELECT_ENERGY_FOR_SPAWN' | 'SPAWN' |
     'SELECT_ENERGY_FOR_MOVE' | 'SELECT_MOVE_TARGET' | 'SELECT_MOVE_DESTINATION' |
-    'SELECT_ENERGY_FOR_ATTACK' | 'SELECT_ATTACK_TARGET' | 'SELECT_ATTACK' |
-    'UNDO';
+    'SELECT_ENERGY_FOR_ATTACK' | 'SELECT_ATTACK_TARGET' | 'SELECT_ATTACK';
 
 type GameStateConfigurations = {
     [gameState in GameState]: {
@@ -57,30 +53,11 @@ type GameStateConfigurations = {
     }
 };
 
-const COLOR_BY_TYPE: { [type in StarterTypes] : string } = {
+const COLOR_BY_TYPE: { [type in StarterTypes]: string } = {
     Grass: 'rgba(119, 204, 85, 0.2)',
     Fire: 'rgba(255, 68, 34, 0.2)',
     Water: 'rgba(51, 153, 255, 0.2)',
 }
-
-/**
- * Limit this to just starter pokemon, and only use their 3 types for some gameplay insteractions
- * randomly spawn one starter/type, and then try to combine types to become stronger and fight for squares
- * agains other types. but you have to play both sides. 
- * or, you pick a color and play with 2 ai that get one of the other types, and you can fight
- * goal could be to try to get as any squares as possible with you pokemon by moving around. 
- * spawn # is how many of the pokemon to spawn. if you spawn duplicates, you will be able to evolve, once they are moved together.
- * evolving makes them stronger all starters would be good to have because of the types that leds itself well to some game play mechnics
- * 
- * Goal is against 2 other plays, try to take over the board and eliminate others squares. idk something like that. will need to have some sort of
- * actual pokemon battle, with the moves data too
- * 
- * okay so instead of using all the pokemon, just use the starters and give them some more data, like moves. and who evolves into who, and the teams they are in
- * 
- * might need to start over and have the user pick a color as the starting board. show opponents on board. left and right side of the bottom space
- * i will show my stuff on top. it will be some stats like points, and the color of the team. might need to change the ui of how move selection should be done
- * 
- */
 
 // TODO - take in game data as props from App.tsx
 export default function Game() {
@@ -98,12 +75,6 @@ export default function Game() {
     // energy
     const [availableEnergy, setAvailableEnergy] = useState<number>(STARTING_ENERGY);
     const [energyToUse, setEnergyToUse] = useState<number>(1);
-    const [energyForSqawn, setEnergyForSqawn] = useState<number>(0);
-    const [enegeryForMoves, setEnergyForMoves] = useState<number>(0);
-
-    // spawn pokemon type selection
-    // const [showTypeSelectionModal, setShowTypeSelectionModal] = useState<boolean>(false);
-    // const [typeToSpawn, setTypeToSpawn] = useState<PokemonType>('Water'); // TODO - change this to something different
 
     // pokemon preview info
     const [showPokemonPreviewInfo, setShowPokemonPreviewInfo] = useState<boolean>(false);
@@ -253,17 +224,6 @@ export default function Game() {
                 inputs: [spawnButton, moveButton, attackButton],
             },
 
-            // Main actions
-            // MOVE: {
-            //     headerText: 'MOVE',
-            //     inputs: [energySlider, navigationButtons],
-            // },
-            // ATTACK: {
-            //     headerText: 'ATTACK',
-            //     inputs: [energySlider, navigationButtons],
-            // },
-
-            // Follow-up actions for Spawn
             SELECT_ENERGY_FOR_SPAWN: {
                 headerText: 'SELECT_ENERGY_FOR_SPAWN',
                 inputs: [energySlider, navigationButtons('SPAWN')],
@@ -273,7 +233,6 @@ export default function Game() {
                 inputs: [],
             },
 
-            // Follow-up actions for Move
             SELECT_ENERGY_FOR_MOVE: {
                 headerText: 'SELECT_ENERGY_FOR_MOVE',
                 inputs: [energySlider, navigationButtons('SELECT_MOVE_TARGET')],
@@ -287,7 +246,6 @@ export default function Game() {
                 inputs: [],
             },
 
-            // Follow-up actions for Attack
             SELECT_ENERGY_FOR_ATTACK: {
                 headerText: 'SELECT_ENERGY_FOR_ATTACK',
                 inputs: [],
@@ -300,12 +258,6 @@ export default function Game() {
                 headerText: 'SELECT_ATTACK',
                 inputs: [],
             },
-
-            // Undo - rename to "energy selection"
-            UNDO: {
-                headerText: '',
-                inputs: [],
-            },
         });
     }, [previousGameState, currentGameState, energyToUse, availableEnergy, gameBoardData])
 
@@ -313,6 +265,9 @@ export default function Game() {
      * Cell-Press Handlers
      *************************************************************************/
 
+    /**
+     * 
+     */
     const handleCellPress = useCallback((row: number, column: number) => {
         const cellData = gameBoardData[row][column];
 
@@ -333,14 +288,16 @@ export default function Game() {
                     for (let j = 0; j < GAMEBOARD_NUM_COLS; j++) {
                         const cell = draft[i][j];
                         if (!cell.pokemonData) {
-                            if ((i-1 === row && j-1 === column) || (i+1 === row && j+1 === column) || (i+1 === row && j-1 === column) || (i-1 === row && j+1 === column) || (i === row && j+1 === column) || (i === row && j-1 === column) || (i+1 === row && j === column) || (i-1 === row && j === column)) {
-                                cell.isPotentialMoveDestination = true;
+                            if ((i - 1 === row && j - 1 === column) || (i + 1 === row && j + 1 === column) || (i + 1 === row && j - 1 === column) || (i - 1 === row && j + 1 === column) || (i === row && j + 1 === column) || (i === row && j - 1 === column) || (i + 1 === row && j === column) || (i - 1 === row && j === column)) {
+                                if (!cell.pokemonData && !cell.ownedByType) {
+                                    cell.isPotentialMoveDestination = true;
+                                }
                             }
                         }
                     }
                 }
             }));
-            setOriginalPokemonCell({ row: row, column: column});
+            setOriginalPokemonCell({ row: row, column: column });
             setPreviousGameState('SELECT_MOVE_TARGET');
             setCurrentGameState('SELECT_MOVE_DESTINATION');
         } else if (currentGameState === 'SELECT_MOVE_DESTINATION' && cellData.isPotentialMoveDestination && originalPokemonCell) {
@@ -362,6 +319,7 @@ export default function Game() {
             }));
 
             const updatedEnergy = energyToUse - 1;
+            setAvailableEnergy(availableEnergy - 1);
             if (updatedEnergy === 0) {
                 setPreviousGameState('SELECT_MOVE_DESTINATION');
                 setCurrentGameState('SELECT_ACTION');
@@ -373,48 +331,12 @@ export default function Game() {
             }
         }
 
-        // } else if (cellData.isPotentialMoveDestination && executableGameAction === 'MOVE') {
-        //     console.log(`[Game][handleCellPress] - PRESS MOVE POKEMON HERE
-        //         originalPokemonCell: ${JSON.stringify(originalPokemonCell)}
-        //     `);
+    }, [currentGameState, previousGameState, energyToUse, availableEnergy, gameBoardData, originalPokemonCell]);
 
-        //     // TODO - this part is not working...................................
-
-        //     setGameBoardData(produce(gameBoardData, draft => {
-        //         for (let i = 0; i < GAMEBOARD_NUM_ROWS; i++) {
-        //             for (let j = 0; j < GAMEBOARD_NUM_COLS; j++) {
-        //                 draft[i][j].isPotentialMoveDestination = false;
-        //             }
-        //         }
-        //         if (originalPokemonCell && originalPokemonCell.row && originalPokemonCell.column) {
-        //             draft[row][column].pokemonData = JSON.parse(JSON.stringify(draft[originalPokemonCell.row][originalPokemonCell.column].pokemonData));
-        //             draft[originalPokemonCell.row][originalPokemonCell.column].pokemonData = undefined;
-        //             console.log(`[Game][handleCellPress][resetting...]`)
-        //         }
-        //     }));
-
-        //     console.log(`[Game][handleCellPress] - updated gameboard data!`);
-
-        //     setOriginalPokemonCell({ row: 0, column: 0});
-
-        //     const newEnergyForMoves = enegeryForMoves - 1;
-        //     setEnergyForMoves(newEnergyForMoves);
-
-        //     if (enegeryForMoves > 0) {
-        //         setGameBoardData(produce(gameBoardData,(draft) => {
-        //             draft[row][column].isPotentialMove = true;
-        //         }));
-        //     }
-        //     console.log(`[Game][handleCellPress] - newEnergyForMoves=${newEnergyForMoves}`)
-        // } else if (cellData.pokemonData) {
-        //     console.log(`[Game][handleCellPress] - CLICK POKEMON PREVIEW`);
-        //     setShowPokemonPreviewInfo(true);
-        //     setPokemonPreviewInfo({ pokemonData: cellData.pokemonData });
-        // }
-    }, [currentGameState, previousGameState, energyToUse, availableEnergy, gameBoardData, originalPokemonCell, enegeryForMoves]);
-
-
-    const handleCellLongPress = (row: number, column: number) => {
+    /**
+     * 
+     */
+    const handleCellLongPress = useCallback((row: number, column: number) => {
         console.log(`[Game][handleCellLongPress]
             row: ${row}
             column: ${column}
@@ -426,8 +348,11 @@ export default function Game() {
             setPokemonPreviewInfo({ pokemonData: cellData.pokemonData });
             setShowPokemonPreviewInfo(true);
         }
-    }
+    }, [gameBoardData]);
 
+    /**
+     * 
+     */
     const onGameActionButtonPress = useCallback((newGameState: GameState) => {
         console.log(`[Game][onGameActionButtonPress]
             newGameState: ${newGameState}
@@ -452,6 +377,9 @@ export default function Game() {
         }
     }, [currentGameState, previousGameState, availableEnergy, energyToUse]);
 
+    /**
+     * 
+     */
     const executeAction = useCallback((newGameState: GameState) => {
         console.log(`[Game][executeAction]
             newGameState=${newGameState}
@@ -496,20 +424,17 @@ export default function Game() {
             setEnergyToUse(1);
             setCurrentGameState('SELECT_ACTION');
             setPreviousGameState('SPAWN');
-        } 
+        }
     }, [currentGameState, previousGameState, gameBoardData, availableEnergy, energyToUse, originalPokemonCell])
-
-    const onSelectStarterType = (type: 'Grass' | 'Fire' | 'Water') => {
-        console.log(`[Game][onSelectStarterType]
-            type: ${type}
-        `);
-        setStarterType(type);
-    }
 
     /**************************************************************************
      * Render methods
      *************************************************************************/
 
+    /**
+     * 
+     * @param pokemonSprite 
+     */
     const renderPokemonImage = (pokemonSprite: string) => {
         if (!pokemonSprite) return null;
 
@@ -525,9 +450,14 @@ export default function Game() {
         );
     }
 
+    /**
+     * 
+     * @param rowIndex 
+     * @param columnIndex 
+     */
     const renderCell = (rowIndex: number, columnIndex: number) => {
-        const cellData: GameboardCellData | null = 
-            gameBoardData && gameBoardData[rowIndex] && gameBoardData[rowIndex][columnIndex] ? 
+        const cellData: GameboardCellData | null =
+            gameBoardData && gameBoardData[rowIndex] && gameBoardData[rowIndex][columnIndex] ?
                 gameBoardData[rowIndex][columnIndex] :
                 null;
 
@@ -568,6 +498,9 @@ export default function Game() {
         );
     }
 
+    /**
+     * 
+     */
     const renderTopSection = () => {
         if (currentGameState && gameStateConfigurations && gameStateConfigurations[currentGameState]) {
             return (
@@ -594,6 +527,9 @@ export default function Game() {
         }
     }
 
+    /**
+     * 
+     */
     const renderBottomSection = () => {
         if (currentGameState && gameStateConfigurations && gameStateConfigurations[currentGameState]) {
             return (
@@ -606,6 +542,9 @@ export default function Game() {
         }
     }
 
+    /**
+     * 
+     */
     const renderPokemonPreviewInfo = () => {
         return (
             <Modal
@@ -631,6 +570,9 @@ export default function Game() {
         );
     }
 
+    /**
+     * 
+     */
     const renderGameboard = () => {
         return (
             Array.from(Array(GAMEBOARD_NUM_ROWS).keys()).map(rowIndex => (
@@ -643,6 +585,9 @@ export default function Game() {
         );
     }
 
+    /**
+     * 
+     */
     const renderStartGameAndStarterTypeSelection = () => {
         console.log(`[Game][renderStartGameAndStarterTypeSelection]
             starterType: ${starterType}
@@ -655,7 +600,7 @@ export default function Game() {
                         <View style={{ opacity: starterType === 'Grass' ? 1 : 0.5 }}>
                             <TouchableOpacity
                                 style={[styles.selectTypeOneColor, { backgroundColor: '#7c5' }]}
-                                onPress={() => onSelectStarterType('Grass')}
+                                onPress={() => setStarterType('Grass')}
                             >
                                 <Text style={styles.starterTypeText}>Grass</Text>
                             </TouchableOpacity>
@@ -663,7 +608,7 @@ export default function Game() {
                         <View style={{ opacity: starterType === 'Fire' ? 1 : 0.5 }}>
                             <TouchableOpacity
                                 style={[styles.selectTypeOneColor, { backgroundColor: '#f42' }]}
-                                onPress={() => onSelectStarterType('Fire')}
+                                onPress={() => setStarterType('Fire')}
                             >
                                 <Text style={styles.starterTypeText}>Fire</Text>
                             </TouchableOpacity>
@@ -671,7 +616,7 @@ export default function Game() {
                         <View style={{ opacity: starterType === 'Water' ? 1 : 0.5 }}>
                             <TouchableOpacity
                                 style={[styles.selectTypeOneColor, { backgroundColor: '#39f' }]}
-                                onPress={() => onSelectStarterType('Water')}
+                                onPress={() => setStarterType('Water')}
                             >
                                 <Text style={styles.starterTypeText}>Water</Text>
                             </TouchableOpacity>
@@ -697,6 +642,9 @@ export default function Game() {
         )
     }
 
+    /**
+     * Main render
+     */
     return (
         <View style={styles.container}>
             {
@@ -714,149 +662,3 @@ export default function Game() {
         </View>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        width: '100%',
-    },
-    actionButtonIcon: {
-        marginRight: 10,
-    },
-    actionButtonIconLeft: {
-        marginLeft: 10,
-    },
-    row: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    cell: {
-        flex: 1,
-        borderLeftWidth: 1,
-        borderTopWidth: 1,
-    },
-    topSection: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        borderRadius: 5,
-        borderColor: 'lightgray',
-        borderWidth: 1,
-        marginLeft: 10,
-        marginRight: 10,
-        marginBottom: 10,
-        marginTop: 25,
-    },
-    statsSection: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly'
-    },
-    statsSeparator: {
-        borderWidth: 0.5,
-        borderColor: 'gray',
-        width: 1,
-    },
-    bottomSection: {
-        flex: 1,
-        flexDirection: 'column',
-        alignSelf: 'stretch',
-        marginLeft: 10,
-        marginRight: 10,
-        marginBottom: 10,
-        marginTop: 10,
-    },
-    actionsHeader: {
-        // borderBottomColor: 'gray',
-        // borderBottomWidth: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-    },
-    actionsButtons: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-    },
-    navigationButtonsSection: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'flex-end',
-        // borderColor: 'black',
-        // borderWidth: 1,
-    },
-    // from modal doc
-    centeredView: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 20,
-    },
-    modalView: {
-        margin: 20,
-        backgroundColor: 'white',
-        borderRadius: 20,
-        paddingVertical: 20,
-        paddingHorizontal: 40,
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
-        elevation: 5,
-    },
-    openButton: {
-        marginTop: 10,
-        backgroundColor: '#F194FF',
-        borderRadius: 20,
-        padding: 10,
-        elevation: 2,
-    },
-    modalButtonText: {
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-    },
-    modalHeaderText: {
-        marginBottom: 10,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    },
-    startGameButtonContainer: {
-        paddingHorizontal: 20
-    },
-    selectTypeForm: {
-        flex: 1,
-        flexDirection: 'column',
-        height: 60
-    },
-    selectTypeColorRow: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    selectTypeOneColor: {
-        borderRadius: 4,
-        height: 40,
-        width: 80,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    starterTypeHeaderText: {
-        marginBottom: 20,
-        fontSize: 20,
-    },
-    starterTypeText: {
-        color: 'white',
-        fontWeight: 'bold',
-    },
-    startGameAndStartTypeSection: {
-        height: 150,
-        paddingHorizontal: 20
-    }
-});
